4df9c37940fbfdb178aeb04501608cb96bc359f8
Emanuelosky <emanueldjesusrivasbrito@gmail.com>
2026-06-09 14:37:09 -0400

    chore: update gitignore and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({ 
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
+		port: 5174,
+		strictPort: true,
 		allowedHosts: ['.loca.lt', '.localtunnel.me']
 	}
 });
